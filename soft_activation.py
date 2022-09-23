5b48ee1cfb95894d26d9ccb29887b53d71c948df
Sammy Peake <peakes@vcu.edu>
2022-09-22 20:16:51 -0400

    hopefully have something

--- soft_activation.py
+++ soft_activation.py
@@ -14,10 +14,6 @@ class perceptron:
         self.normalized_df[0] = (self.df[0]-self.df[0].min())/(self.df[0].max()-self.df[0].min())
         self.normalized_df[1] = (self.df[1]-self.df[1].min())/(self.df[1].max()-self.df[1].min())
 
-    #performance reasons
-    def vectorize(self):
-        self.normalized_df.to_numpy()
-
     def _output_soft(self,net,k):
         return 2/(1+np.exp(-2*k*net)) - 1
 
@@ -26,11 +22,35 @@ class perceptron:
 
     def _select_test(self):
         pass
-
-    def _train_soft(self,np,num_inp,gain,TE=0,k=0.0,weights = [],pattern=[]):
+    #use weights from the first assignment?
+    def _train_soft(self,nump,gain,iterations = 5000,lc = 0.1,TE=0,k=0.0,weights = [0,0,0],pattern=[]):
         #selected data
         td = self._select_tr()
-        ni = 5000
+        td = self.normalized_df
+        ni = 3
+        k = gain
+        w = weights
+        nump = td.shape[0]
+        alpha = lc
+        desired_out = [] 
+        #for designated number of iterations
+        for ite in range(0,iterations):
+            out = [0,0]
+            #Loop over the amount of rows in the data
+            for (idx,row) in enumerate(nump):
+                net = 0
+                for i in range(0,ni):
+                    net = net + weights[i]*td[row][i]*alpha
+                
+                out[row] = self._output_soft(net,k)
+                err = desired_out[row] - out[row]
+                if err <= TE:
+                    break 
+                learn = alpha*err
+                #update the weights
+                for i in range(0,ni):
+                    w[i] = w[i] + learn * td[row][i]
+
 
     def _test(self):
         test = self._select_test()
@@ -46,3 +66,11 @@ class perceptron:
         plt.ylabel('weight') 
         plt.title('Weight vs Cost A')
         plt.savefig('../plots/plot_a.png')
+
+def main():
+    #A: y = 1.03353 - 1.07843x
+    # => y + 1.07843x < 1.03353
+    pass
+
+if __name__ == '__main__':
+    main()
